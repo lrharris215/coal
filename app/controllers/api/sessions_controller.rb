cef7e9503c99e4ac594903b29ae7ad70fb489eca
Lane Harris <lrharris215@gmail.com>
2021-03-15 14:21:19 -0400

    Adds user jbuilder page

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,3 +1,21 @@
 class SessionsController < ApplicationController 
-    
+    def create
+        @user = User.find_by_credentials(
+            params[:user][:username],
+            params[:user][:password]
+        )
+        if @user
+            login!(user)
+            render 'api/users/show'
+        end
+    end
+
+    def destroy
+        if current_user
+            logout!
+            render json: {}
+        else
+            render json: ["You're not logged in!"], status: 404
+        end
+    end
 end
--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
     def create 
         @user = User.new(user_params)
         if @user.save
-            render json: @user
+            render 'api/users/show'
         else
             render json: @user.errors.full_messages, status: 422
         end
