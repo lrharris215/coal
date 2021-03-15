84f090d71319fbcb4cd94b92053a2a9242aee981
Lane Harris <lrharris215@gmail.com>
2021-03-15 14:09:53 -0400

    Adds UsersController#create

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,3 +1,17 @@
 class UsersController < ApplicationController
-    
+
+    def create 
+        @user = User.new(user_params)
+        if @user.save
+            render json: @user
+        else
+            render json: @user.errors.full_messages, status: 422
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
 end
