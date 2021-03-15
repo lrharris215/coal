7d6384e7661f5f07e5517c6f7c764a1f2f6e7c9a
Lane Harris <lrharris215@gmail.com>
2021-03-15 15:04:06 -0400

    Adds session_actions, sessions_reducer

--- frontend/coal.jsx
+++ frontend/coal.jsx
@@ -1,1 +1,31 @@
-// entry file
+//entry file
+import React from 'react';
+import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+import Root from './components/root'
+import { createNewUser, loginUser, logoutUser } from './actions/session_actions'
+
+document.addEventListener("DOMContentLoaded", () => {
+    let preloadedState = {};
+    
+    if(window.currentUser) {
+        preloadedState = {
+            entities: {
+                users: {
+                    [window.currentUser.id]: window.currentUser
+                }
+            },
+            session: { id: window.currentUser.id}
+        }
+    }
+    const root = document.getElementById("root");
+    const store = configureStore(preloadedState);
+
+    window.store = store;
+    window.createNewUser = createNewUser;
+    window.loginUser = loginUser;
+    window.logoutUser = logoutUser;
+
+    ReactDOM.render(<Root store={store} />, root)
+})
+
