ce82f3d88a340ae8a89596701bd46eb9dfdcffcd
Lane Harris <lrharris215@gmail.com>
2021-03-16 16:59:44 -0400

    Creates rdropdown menu and container

--- frontend/components/dropdowns/dropdown.jsx
+++ frontend/components/dropdowns/dropdown.jsx
@@ -1,16 +1,40 @@
 import React from 'react';
 
-export default (props) => {
-    return (
-        <div className={props.klassName}>
-            <ul>
-                {props.listItems.map((item) => {
-                    return (
-                        <li>{item}</li>
-                    )
-                    })
-                }
-            </ul>
-        </div>
-    )
+
+class Dropdown extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state= {
+            active: false,
+        }
+        this.toggleActive = this.toggleActive.bind(this);
+    }
+
+    toggleActive() {
+        debugger
+        this.setState({
+            active: !this.state.active,
+        })
+    }
+
+    render() {
+        const activeClass = (this.state.active) ? "active" : "hidden";
+        return (
+            <div className={this.props.klassName}>
+                <div onClick={this.toggleActive}>{this.props.titleItem}</div>
+                <div className={activeClass}>
+                    <ul>
+                        {this.props.listItems.map((item) => {
+                            return (
+                                <li>{item}</li>
+                            )
+                            })
+                        }
+                    </ul>
+                </div>
+            </div>
+        )
+    }
 }
+
+export default Dropdown;
--- frontend/components/main_header/main_header.jsx
+++ frontend/components/main_header/main_header.jsx
@@ -1,53 +1,68 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Link } from 'react-router-dom';
+import HeaderDropDownContainer from "../dropdowns/header_drop_down"
 
 
-export default (props) => {
-    const installButtonColor = props.loggedIn ? "grey" : "orange"
-    return (
+class MainHeader extends React.Component {
+        constructor(props){
+            super(props);
+        }
+   
+        render () {
+            const installButtonColor = this.props.loggedIn ? "grey" : "orange";
+            return (
 
-        <div className="main-header">
-            <Link className="logo-container" to="/">
-            
-            <div className="logo">
-                <i className="fas fa-cogs"></i>
-            </div>
-            <h1>Coal</h1>
-            
-            </Link>
-
-            <nav className="main-header-nav">
-                <ul>
-                    <li><a href="">Store</a></li>
-                    <li><a href="">Community</a></li>
-                    {props.loggedIn ? <li><a href="">{props.username}</a></li> : <li><a href="">About</a></li>}
-
-                    <li><a href="">Chat</a></li>
-                    <li><a href="">Support</a></li>
-                </ul>
-            </nav>
-
-            <div className="right-main-header">
-                <div className={`install-button ${installButtonColor}`}>
-                    <i className="fas fa-download"></i>
-                    Install Coal</div>
-                {props.loggedIn ? <div className="loggedIn-right-header">
-                    <div className="envelope">
-                <i className="fas fa-envelope"></i>
-                    </div>
-                    <p>{props.username}<i className="fas fa-caret-down"></i></p>
-                    <div className="profile-pic">
-                        <img  src={window.cubeURL} alt="Profile pic" />
-                    </div>
-                <button onClick={props.logoutUser}>Log Out</button>  
+            <div className="main-header">
+                <Link className="logo-container" to="/">
+                
+                <div className="logo">
+                    <i className="fas fa-cogs"></i>
                 </div>
-                : 
-                <div>
-                    <Link to="/login">login</Link> 
-                    <p>| language</p>
-                </div> }
+                <h1>Coal</h1>
+                
+                </Link>
+
+                <nav className="main-header-nav">
+                    <ul>
+                        <li><a href="">Store</a></li>
+                        <li><a href="">Community</a></li>
+                        {this.props.loggedIn ? <li><a href="">{this.props.username}</a></li> : <li><a href="">About</a></li>}
+
+                        <li><a href="">Chat</a></li>
+                        <li><a href="">Support</a></li>
+                    </ul>
+                </nav>
+
+                <div className="right-main-header">
+                    <div className={`install-button ${installButtonColor}`}>
+                        <i className="fas fa-download"></i>
+                        Install Coal</div>
+                    {this.props.loggedIn ? <div className="loggedIn-right-header">
+                        <div className="envelope">
+                    <i className="fas fa-envelope"></i>
+                        </div>
+                       
+                            <HeaderDropDownContainer titleItem={<p>{this.props.username}<i className="fas fa-caret-down"></i></p>}listItems={
+                            [<a href="">View profile</a>, 
+                            <a href="">Account details</a>, 
+                            <button onClick={this.props.logoutUser}>Log Out</button>, 
+                            <a href="">Preferences</a>, 
+                            <a href="">Change language</a>]}/>
+                        <div className="profile-pic">
+                            <img  src={window.cubeURL} alt="Profile pic" />
+                        </div>
+                    
+                    </div>
+                    : 
+                    <div>
+                        <Link to="/login">login</Link> 
+                        <p>| language</p>
+                    </div> }
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
+
+export default MainHeader;
