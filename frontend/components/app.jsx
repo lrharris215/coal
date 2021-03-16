4dae8e27e1ef7d1de507ba366dd7d1912980f698
Lane Harris <lrharris215@gmail.com>
2021-03-16 10:12:55 -0400

    Header components complete, user can Log-in and Log-out

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,16 +1,15 @@
-import { Route, Switch, Link} from 'react-router-dom'
-import React from 'react'
-import {AuthRoute, ProtecedRoute} from '../util/route_util'
-import LogInFormContainer from '../components/session_form/login-form-container'
-import SignUpFormContainer from "../components/session_form/signup-form-container"
+import { Route, Switch, Link} from 'react-router-dom';
+import React from 'react';
+import {AuthRoute, ProtecedRoute} from '../util/route_util';
+import LogInFormContainer from '../components/session_form/login-form-container';
+import SignUpFormContainer from "../components/session_form/signup-form-container";
+import MainHeader from "./main_header/main_header_container"
 
 const App = () => {
     return (
     <div>
         <header>
-            <Link to='/' className="header-link">
-                <h2>Coal</h2>
-            </Link>
+            <MainHeader />
             <AuthRoute path="/login" component={LogInFormContainer}/>
             <AuthRoute path="/signup" component={SignUpFormContainer} />
         </header>
--- frontend/components/main_header/main_header.jsx
+++ frontend/components/main_header/main_header.jsx
@@ -1,20 +1,37 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Link } from 'react-router-dom'
 
 export default (props) => {
     return (
         <div className="main-header">
-            <div className="logo"></div>
+            <div className="logo-container">
+            <Link to= "/">
+            <div className="logo">
+                <i className="fas fa-cogs"></i>
+                <span>Coal</span>
+            </div>
+            </Link>
+            </div>
 
-            <nav>
+            <nav className="main-header-nav">
                 <ul>
                     <li>Store</li>
                     <li>Community</li>
-                    <li>{this.props.username}</li>
+                    {props.loggedIn ? <li>{props.username}</li> : <li>About</li>}
+
                     <li>Chat</li>
                     <li>Support</li>
                 </ul>
             </nav>
+
+            <div className="right-main-header">
+                {props.loggedIn ? <button onClick={props.logoutUser}>Log Out</button>  : 
+                <div>
+                    <Link to="/login">Sign In</Link>
+                    <Link to="/signup">Sign Up</Link>
+                </div> }
+            </div>
         </div>
     )
 }
