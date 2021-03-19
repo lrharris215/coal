a5ca6c74cbba9c48a47ae024724542df29a9820c
Lane Harris <lrharris215@gmail.com>
2021-03-19 08:52:04 -0400

    Adds gamesReducer

---
+++ frontend/reducers/games_reducer.js
@@ -0,0 +1,13 @@
+import { RECEIVE_GAME, RECEIVE_GAMES } from '../actions/games_actions';
+
+export default (oldState, action) => {
+    switch(action.type) {
+        case RECEIVE_GAMES:
+            return Object.assign({}, action.games);
+        case RECEIVE_GAME: 
+            return Object.assign({}, oldState, {[action.game.id]: action.game});
+        default:
+            return oldState;
+    }
+    
+}
