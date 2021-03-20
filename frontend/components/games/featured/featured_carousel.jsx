f8eeebaf5c6cbad396351d4962bca76db4c13ba1
Lane Harris <lrharris215@gmail.com>
2021-03-20 19:43:29 -0400

    styled left arrow, made arrows work

--- frontend/components/games/featured/featured_carousel.jsx
+++ frontend/components/games/featured/featured_carousel.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 class FeaturedCarousel extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             activeGameIdx: null,
+            activePicIdx: 0,
         };
     }
 
     componentDidMount() {
         this.props.requestFeaturedGames();
+        setInterval(() => {
+            console.log('wooof!');
+            this.setState({
+                activeGameIdx:
+                    this.state.activeGameIdx === this.props.games.length - 1 ? 0 : this.state.activeGameIdx + 1,
+            });
+        }, 5 * 1000);
     }
     componentDidUpdate(oldProps, oldState) {
         if (oldState.activeGameIdx === null && this.props.games.length > 0) {
@@ -20,15 +28,23 @@ class FeaturedCarousel extends React.Component {
     }
     render() {
         const { games } = this.props;
+
         return (
             <div className="featured-carousel-outer-div">
-                <h2>Featured and Recommended</h2>
+                <h2>Featured & Recommended</h2>
                 <div>
-                    <div className="left-arrow">
+                    <div
+                        className="left-arrow"
+                        onClick={() => {
+                            this.setState((state) => ({
+                                activeGameIdx: state.activeGameIdx === 0 ? games.length - 1 : state.activeGameIdx - 1,
+                            }));
+                        }}>
                         <i className="fas fa-chevron-left"></i>
                     </div>
                     <ul>
                         {games.map((game, idx) => {
+                            const allImages = Object.values(game.gameImages);
                             const titleCard = Object.values(game.gameImages).filter(
                                 (gameImage) => gameImage.img_type === 'title-card'
                             );
@@ -37,38 +53,60 @@ class FeaturedCarousel extends React.Component {
                             );
                             return (
                                 <div className={this.state.activeGameIdx === idx ? 'active' : 'hidden'}>
-                                    <div className="featured-carousel-inner-div">
-                                        <div className="big-image">
-                                            <img src={titleCard[0].img_url} alt={`${game.title}-title-card`} />
+                                    <Link to={`api/games/${game.id}`}>
+                                        <div className="featured-carousel-inner-div">
+                                            <div className="big-image">
+                                                <img
+                                                    src={allImages[this.state.activePicIdx].img_url}
+                                                    alt={`${game.title}-title-card`}
+                                                />
+                                            </div>
+                                            <div className="featured-right-side">
+                                                <h2 className="title">{game.title}</h2>
+                                                <div className="four-pic-box">
+                                                    {screenshots.map((screenshot) => {
+                                                        return (
+                                                            <div
+                                                                className="screenshot-div"
+                                                                onMouseEnter={() => {
+                                                                    this.setState((state) => ({
+                                                                        activePicIdx: allImages.indexOf(screenshot),
+                                                                    }));
+                                                                }}
+                                                                onMouseLeave={() => {
+                                                                    this.setState((state) => ({
+                                                                        activePicIdx: 0,
+                                                                    }));
+                                                                }}>
+                                                                <img
+                                                                    src={screenshot.img_url}
+                                                                    alt={`${game.title}-screenshot`}
+                                                                />
+                                                            </div>
+                                                        );
+                                                    })}
+                                                </div>
+                                                <div className="recommended">
+                                                    Recommended because you played games tagged with <div>category</div>
+                                                </div>
+                                                <div className="featured-bottom-row">
+                                                    <p>${game.price / 100}</p>
+                                                    <i className="fab fa-windows"></i>
+                                                </div>
+                                            </div>
                                         </div>
-                                        <div className="featured-right-side">
-                                            <h2 className="title">{game.title}</h2>
-                                            <div className="four-pic-box">
-                                                {screenshots.map((screenshot) => {
-                                                    return (
-                                                        <div className="screenshot-div">
-                                                            <img
-                                                                src={screenshot.img_url}
-                                                                alt={`${game.title}-screenshot`}
-                                                            />
-                                                        </div>
-                                                    );
-                                                })}
-                                            </div>
-                                            <div className="recommended">
-                                                Recommended because you played games tagged with <div>category</div>
-                                            </div>
-                                            <div className="featured-bottom-row">
-                                                <p>${game.price / 100}</p>
-                                                <i className="fab fa-windows"></i>
-                                            </div>
-                                        </div>
-                                    </div>
+                                    </Link>
                                 </div>
                             );
                         })}
                     </ul>
-                    <div className="right-arrow">
+                    <div
+                        className="right-arrow"
+                        onClick={() => {
+                            this.setState((state) => ({
+                                activeGameIdx: state.activeGameIdx === games.length - 1 ? 0 : state.activeGameIdx + 1,
+                            }));
+                        }}>
                         <i className="fas fa-chevron-right"></i>
                     </div>
                 </div>
