bc466cea23ca7883ba41a1b6385d89ef820bbb71
Lane Harris <lrharris215@gmail.com>
2021-03-21 13:10:12 -0400

    added screenshots to game-detail page

--- frontend/components/games/show/game_detail.jsx
+++ frontend/components/games/show/game_detail.jsx
@@ -4,19 +4,27 @@ class GameDetail extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            activePic: null,
+            activePicIdx: 0,
         };
     }
     componentDidMount() {
         this.props.requestOneGame(this.props.gameId);
     }
-
+    componentDidUpdate(oldProps, oldState) {
+        debugger;
+        if (oldState.activePicIdx === null && this.props.games.length > 0) {
+            this.setState({
+                activeGameIdx: 0,
+            });
+        }
+    }
     render() {
         const { gameId, games } = this.props;
         const game = games && games[gameId];
-        debugger;
+
         const titleCard = game && Object.values(game.gameImages).filter((image) => image.img_type === 'title-card');
         const screenshots = game && Object.values(game.gameImages).filter((image) => image.img_type === 'screenshot');
+        debugger;
         if (!game) {
             return <div></div>;
         }
@@ -24,7 +32,9 @@ class GameDetail extends React.Component {
             <div className="game-detail-container">
                 <div className="game-detail-header">
                     <div className="gd-header-left">
-                        <p>All Games > Indie Games > {game.title}</p>
+                        <p>
+                            All Games {'>'} Indie Games {'>'} {game.title}
+                        </p>
                         <h2>{game.title}</h2>
                     </div>
                     <div className="gd-header-right">
@@ -33,8 +43,23 @@ class GameDetail extends React.Component {
                 </div>
                 <div className="game-detail-main">
                     <div className="game-detail-left">
-                        <div className="big-picture"></div>
-                        <div className="screenshots"></div>
+                        <div className="big-picture">
+                            <img
+                                src={screenshots[this.state.activePicIdx].img_url}
+                                alt={`screenshot from ${game.title}`}
+                            />
+                        </div>
+                        <div className="screenshots">
+                            {screenshots.map((screenshot, idx) => {
+                                return (
+                                    <img
+                                        className="screenshot"
+                                        src={screenshot.img_url}
+                                        alt={`screenshot #${idx + 1} from ${game.title}`}
+                                    />
+                                );
+                            })}
+                        </div>
                     </div>
                     <div className="game-detail-right">
                         <div className="title-card">
@@ -58,7 +83,14 @@ class GameDetail extends React.Component {
                                 <p>{game.publisher}</p>
                             </div>
                         </div>
-                        <div className="popular-tags"></div>
+                        <div className="popular-tags">
+                            <p>Popular user-defined tags for this product:</p>
+                            <div className="tags">
+                                <div className="tag">Tag-1</div>
+                                <div className="tag">Tag-2</div>
+                                <div className="tag">Tag-3</div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
