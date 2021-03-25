6282e38711b3baeec95ef8da6e026bc2760834b1
Lane Harris <lrharris215@gmail.com>
2021-03-25 10:07:44 -0400

    title-card shows up in library

--- frontend/components/games_library/library_index_item.jsx
+++ frontend/components/games_library/library_index_item.jsx
@@ -3,10 +3,12 @@ import React from 'react';
 export default (props) => {
     const game = props.game;
     debugger;
-    // const titleCard = game.gameImages.filter((image) => image.img_type === 'title-card');
+    const titleCard = game.titleCard[0];
     return (
         <div className="library-index-item">
-            <div className="titleCard">{/* <img src={titleCard.img_url} /> */}</div>
+            <div className="titleCard">
+                <img src={titleCard.img_url} />
+            </div>
             <div>{game.id}</div>
             <div className="title">{game.title}</div>
             <div className="hours"></div>
