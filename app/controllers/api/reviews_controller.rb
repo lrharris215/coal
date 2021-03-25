699f36c49c832c2aafde7059dfe729e76ead4a94
Lane Harris <lrharris215@gmail.com>
2021-03-25 16:59:56 -0400

    Adds controller methods to reviews controller

--- app/controllers/api/reviews_controller.rb
+++ app/controllers/api/reviews_controller.rb
@@ -1,3 +1,22 @@
 class Api::ReviewsController < ApplicationController 
-    
+    def game_reviews
+        @reviews = Review.where(game_id: params[:game_id])
+        render json: @reviews
+    end
+
+    def new_game_review
+        @review = Review.new(review_params)
+        if  @review.save!
+           
+            render json: @review
+        else
+            render json: @review.errors.full_messages, status 400;
+        end
+    end
+
+    private 
+
+    def review_params 
+        params.require(:review).permit(:game_id, :author_id, :title, :body)
+    end
 end
