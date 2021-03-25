10d83869cdbc472bf8ce1d46d93c5aef2a82a773
Lane Harris <lrharris215@gmail.com>
2021-03-25 16:39:35 -0400

    created backend routes for reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,7 +2,11 @@ class Review < ApplicationRecord
     validates :author_id, :game_id, presence: true
      validates :author_id, uniqueness: {scope: :game_id}
 
-     belongs_to: game,
+     belongs_to :game,
      foreign_key: :game_id,
      class_name: :Game
+
+     belongs_to :author,
+     foreign_key: :author_id,
+     class_name: :User
 end
