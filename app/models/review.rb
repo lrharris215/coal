898e20468ff79efd1fb9b152a42056a61ef7ae49
Lane Harris <lrharris215@gmail.com>
2021-05-25 10:58:59 -0400

    added validation to review model to check if author owns the game before saving the review to the DB

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,7 @@
 class Review < ApplicationRecord
     validates :author_id, :game_id, presence: true
-     validates :author_id, uniqueness: {scope: :game_id}
+    validates :author_id, uniqueness: {scope: :game_id}
+    validate :doesAuthorOwnGame?
 
      belongs_to :game,
      foreign_key: :game_id,
@@ -9,4 +10,13 @@ class Review < ApplicationRecord
      belongs_to :author,
      foreign_key: :author_id,
      class_name: :User
+     
+    
+     def doesAuthorOwnGame?
+        if !self.author.games.include?(self.game)
+            errors.add(:game_id, "You must own the game to leave a review")
+        end
+
+     end
+
 end
