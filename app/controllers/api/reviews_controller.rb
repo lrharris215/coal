e41d47e835ab76ec1af6016a4958b081f98337ee
Lane Harris <lrharris215@gmail.com>
2021-05-25 14:01:21 -0400

    Changed conditional in ReviewsController#new_game_review to see if new review is valid

--- app/controllers/api/reviews_controller.rb
+++ app/controllers/api/reviews_controller.rb
@@ -6,8 +6,9 @@ class Api::ReviewsController < ApplicationController
 
     def new_game_review
         @review = Review.new(review_params)
-        if  @review.save!
-           
+     
+        if  @review.valid?
+            @review.save!
             render :show
         else
             render json: @review.errors.full_messages, status: 400
